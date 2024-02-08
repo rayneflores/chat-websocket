752332ce41e4e76dd021b735d8c7da30dbcd4255
Rayne Flores <raynitoflores@gmail.com>
2024-02-08 09:38:04 -0400

    Initial commit

---
+++ src/main/java/chatwebsocket/service/ChatHandler.java
@@ -0,0 +1,33 @@
+package chatwebsocket.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Service
+public class ChatHandler extends TextWebSocketHandler {
+
+    private final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession webSocketsession : sessions) {
+            webSocketsession.sendMessage(message);
+        }
+    }
+}
---
+++ src/test/java/chatwebsocket/ChatWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package chatwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
